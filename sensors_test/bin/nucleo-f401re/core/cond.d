a3856920fb7942af958030e8e97816be40868a56
gibr098 <sirico.1810153@studenti.uniroma1.it>
2022-04-27 15:57:36 +0200

    added final sensor_test

---
+++ sensors_test/bin/nucleo-f401re/core/cond.d
@@ -0,0 +1,163 @@
+/home/gabriele/ultrasonic_sensor_test/bin/nucleo-f401re/core/cond.o: \
+ /home/gabriele/RIOT/core/cond.c \
+ /home/gabriele/ultrasonic_sensor_test/bin/nucleo-f401re/riotbuild/riotbuild.h \
+ /home/gabriele/RIOT/core/include/cond.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /home/gabriele/RIOT/core/include/list.h \
+ /home/gabriele/RIOT/core/include/mutex.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/gabriele/RIOT/core/include/kernel_defines.h \
+ /home/gabriele/RIOT/core/include/thread.h \
+ /home/gabriele/RIOT/core/include/clist.h \
+ /home/gabriele/RIOT/core/include/cib.h \
+ /home/gabriele/RIOT/core/include/assert.h \
+ /home/gabriele/RIOT/core/include/msg.h \
+ /home/gabriele/RIOT/core/include/sched.h /usr/include/newlib/inttypes.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/_ansi.h \
+ /home/gabriele/RIOT/core/include/native_sched.h \
+ /home/gabriele/RIOT/cpu/stm32/include/cpu_conf.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/cpu_conf_common.h \
+ /home/gabriele/RIOT/build/stm32/cmsis/f4/Include/stm32f4xx.h \
+ /home/gabriele/RIOT/build/stm32/cmsis/f4/Include/stm32f401xe.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/vendor/core_cm4.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/vendor/cmsis_version.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/vendor/cmsis_compiler.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/vendor/cmsis_gcc.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/vendor/mpu_armv7.h \
+ /home/gabriele/RIOT/build/stm32/cmsis/f4/Include/system_stm32f4xx.h \
+ /home/gabriele/RIOT/cpu/stm32/include/irqs/f4/irqs.h \
+ /home/gabriele/RIOT/core/include/thread_config.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/thread_arch.h \
+ /home/gabriele/RIOT/core/include/irq.h \
+ /home/gabriele/RIOT/cpu/cortexm_common/include/irq_arch.h \
+ /home/gabriele/RIOT/core/include/irq.h \
+ /home/gabriele/RIOT/core/include/mutex.h \
+ /home/gabriele/RIOT/core/include/thread.h \
+ /home/gabriele/RIOT/core/include/debug.h /usr/include/newlib/stdio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /home/gabriele/RIOT/core/include/assert.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h
+
+/home/gabriele/ultrasonic_sensor_test/bin/nucleo-f401re/riotbuild/riotbuild.h:
+
+/home/gabriele/RIOT/core/include/cond.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/home/gabriele/RIOT/core/include/list.h:
+
+/home/gabriele/RIOT/core/include/mutex.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/home/gabriele/RIOT/core/include/kernel_defines.h:
+
+/home/gabriele/RIOT/core/include/thread.h:
+
+/home/gabriele/RIOT/core/include/clist.h:
+
+/home/gabriele/RIOT/core/include/cib.h:
+
+/home/gabriele/RIOT/core/include/assert.h:
+
+/home/gabriele/RIOT/core/include/msg.h:
+
+/home/gabriele/RIOT/core/include/sched.h:
+
+/usr/include/newlib/inttypes.h:
+
+/usr/include/newlib/nano/newlib.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/usr/include/newlib/_ansi.h:
+
+/home/gabriele/RIOT/core/include/native_sched.h:
+
+/home/gabriele/RIOT/cpu/stm32/include/cpu_conf.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/cpu_conf_common.h:
+
+/home/gabriele/RIOT/build/stm32/cmsis/f4/Include/stm32f4xx.h:
+
+/home/gabriele/RIOT/build/stm32/cmsis/f4/Include/stm32f401xe.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/vendor/core_cm4.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/vendor/cmsis_version.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/vendor/cmsis_compiler.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/vendor/cmsis_gcc.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/vendor/mpu_armv7.h:
+
+/home/gabriele/RIOT/build/stm32/cmsis/f4/Include/system_stm32f4xx.h:
+
+/home/gabriele/RIOT/cpu/stm32/include/irqs/f4/irqs.h:
+
+/home/gabriele/RIOT/core/include/thread_config.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/thread_arch.h:
+
+/home/gabriele/RIOT/core/include/irq.h:
+
+/home/gabriele/RIOT/cpu/cortexm_common/include/irq_arch.h:
+
+/home/gabriele/RIOT/core/include/irq.h:
+
+/home/gabriele/RIOT/core/include/mutex.h:
+
+/home/gabriele/RIOT/core/include/thread.h:
+
+/home/gabriele/RIOT/core/include/debug.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/home/gabriele/RIOT/core/include/assert.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/include/newlib/sys/_pthreadtypes.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
--- ultrasonic_sensor_test/bin/nucleo-f401re/shell_commands/sc_ps.d
+++
@@ -1,8 +0,0 @@
-/home/gabriele/Desktop/Iot-Group-Project/ultrasonic_sensor_test/bin/nucleo-f401re/shell_commands/sc_ps.o: \
- /home/gabriele/RIOT/sys/shell/commands/sc_ps.c \
- /home/gabriele/Desktop/Iot-Group-Project/ultrasonic_sensor_test/bin/nucleo-f401re/riotbuild/riotbuild.h \
- /home/gabriele/RIOT/sys/include/ps.h
-
-/home/gabriele/Desktop/Iot-Group-Project/ultrasonic_sensor_test/bin/nucleo-f401re/riotbuild/riotbuild.h:
-
-/home/gabriele/RIOT/sys/include/ps.h:
